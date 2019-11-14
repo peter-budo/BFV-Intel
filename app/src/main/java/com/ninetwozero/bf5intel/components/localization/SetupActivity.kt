49bb7b298d651f687c93847b7237bf6071f5605e
Peter Miklosko <peter.budo@gmail.com>
2019-11-14 06:32:57 +0000

    Updated gradle plugin. Trying to create navigation pattern

--- app/src/main/java/com/ninetwozero/bf5intel/components/localization/SetupActivity.kt
+++ app/src/main/java/com/ninetwozero/bf5intel/components/localization/SetupActivity.kt
@@ -2,8 +2,11 @@ package com.ninetwozero.bf5intel.components.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 import com.ninetwozero.bf5intel.R
 import com.ninetwozero.bf5intel.components.localization.fragments.LanguageFragment
+import com.ninetwozero.bf5intel.components.localization.fragments.PermissionsFragment
 
 class SetupActivity : AppCompatActivity() {
 
@@ -11,10 +14,20 @@ class SetupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, LanguageFragment.newInstance())
-                .commitNow()
+            showScreen(R.layout.language_fragment)
         }
     }
 
+
+    fun showScreen(@LayoutRes layoutId: Int) {
+        val fragmentToOpen: Fragment = when(layoutId) {
+            R.layout.language_fragment -> LanguageFragment.newInstance()
+            R.layout.permissions_fragment -> PermissionsFragment.newInstance()
+            else -> Fragment() //FIXME
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragmentToOpen)
+            .commitNow()
+    }
 }
